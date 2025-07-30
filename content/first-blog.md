08bb96d5770be0bed200b6823bf828aa3a66f020
Andy34G7 <andy34g7@gmail.com>
2025-07-30 22:52:47 +0530

    feat: add blog pages

---
+++ content/first-blog.md
@@ -0,0 +1,7 @@
+---
+title: "My First Blog Post"
+date: "2025-07-30"
+layout: "post"
+---
+
+This is the content of my first post. Welcome to the blog!
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import markdown
 from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
 
 TEMPLATE_DIR = "templates"
 CONFIG_FILE = "config.yaml"
@@ -16,9 +17,11 @@ env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 
 templates = {
     "main": env.get_template('main.html'),
-    # "blog_post": env.get_template('blog_post.html'),
-    # "blog_link_stash": env.get_template('blog_link_statsh.html')
+    "post": env.get_template('post.html'),
+    "blog": env.get_template('blog.html')
 }
+
+allposts = []
 
 for file in os.listdir(CONTENT_DIR):
     if not file.endswith(".md"):
@@ -29,7 +32,7 @@ for file in os.listdir(CONTENT_DIR):
         file_content = f.read()
     parts = file_content.split("---",2)
     page_config = yaml.safe_load(parts[1])
-    markdown_data = parts[2]
+    markdown_data = parts[2]    
 
     html_data = markdown.markdown(markdown_data)
     layout = page_config.get('layout')
@@ -42,6 +45,15 @@ for file in os.listdir(CONTENT_DIR):
     )
 
     output_filename = os.path.splitext(file)[0] + '.html'
+
+    page_config['url'] = '/' + output_filename
+    if page_config.get('layout') == 'post':
+        allposts.append(page_config)
+    if layout == 'blog':
+        allposts.sort(key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d'), reverse=True)
+        final_html = template.render(site=site_config, page=page_config, posts=allposts)
+    else:
+        final_html = template.render(site=site_config, page=page_config, content=html_data)
     output_path = os.path.join(OUTPUT_DIR, output_filename)
     with open(output_path, 'w') as f:
         f.write(final_html)
