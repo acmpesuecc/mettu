e47f8371dced2f543844fcb7e019d33af597f67d
saiarjun546 <saiarjun546@outlook.com>
2025-10-18 00:41:26 +0530

    made the template loading dynamic from static

--- src/tempelate.py
+++ src/tempelate.py
@@ -4,11 +4,16 @@ import os
 
 def render_page(page_config, html_data, site_config, templates, all_posts=None, OUTPUT_DIR='.'):
     layout = page_config.get("layout")
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.getenv("TEMPLATE_DIR", "templates")))
+    try:
+        template = env.get_template(f'{layout}.html')
+    except jinja2.exceptions.TemplateNotFound:
+        print(f'Template {layout}.html not found. Skipping build')
+        return
     if layout not in templates:
         print("Error: Template not found. Skipping build.")
         return
 
-    template = templates[layout]
 
     render_details = {"site": site_config, "page": page_config, "content": html_data}
     if layout == "blog" and all_posts is not None:
